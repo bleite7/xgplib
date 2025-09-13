7925d503dbd921583cc01afcdf5c5a7b8f2f0edc
Bruno Leite <leite.bruno5@gmail.com>
2025-09-13 13:28:42 -0300

    Remove unused global using directives in GlobalUsings.cs for cleaner code

--- src/XgpLib.SyncService/GlobalUsings.cs
+++ src/XgpLib.SyncService/GlobalUsings.cs
@@ -1,7 +1,2 @@
-﻿global using XgpLib.SyncService.Application.Interfaces.Services;
-global using XgpLib.SyncService.Application.UseCases;
-global using XgpLib.SyncService.Domain.Interfaces.Repositories;
-global using XgpLib.SyncService.Infrastructure.HttpHandlers;
-global using XgpLib.SyncService.Infrastructure.Interfaces.Services;
-global using XgpLib.SyncService.Infrastructure.Services;
+﻿global using XgpLib.SyncService.Application.UseCases;
 global using XgpLib.SyncService.Workers;
